3044ae7089e6d35b9bbde3c81242d8addebe8d1b
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-10 23:00:44 +0530

    [Anshuman] Add: Create Inventory Data Managment

---
+++ ObjectOrientedPracticeProblem/InventoryDataManagment/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPracticeProblem.InventoryDataManagment
+{
+    internal class Inventory
+    {
+        List<Details> detail = new List<Details>();
+        public void ReadJsonFile(string fileName)
+        {
+            using StreamReader reader = new StreamReader(fileName);
+            {
+                var json = reader.ReadToEnd();
+                this.detail = JsonConvert.DeserializeObject<List<Details>>(json);
+                Console.WriteLine("Name" + "\t" + "Weight" + "\t" + "Price" + "\t" + "PricePerkg");
+                foreach (var data in detail)
+                {
+                    Console.WriteLine(data.Name + "\t" + data.Weight + "\t" + data.Price + "\t" + data.Weight * data.Price);
+                }
+            }
+        }
+    }
+}
--- ObjectOrientedPracticeProblem/Program.cs
+++ ObjectOrientedPracticeProblem/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ObjectOrientedPracticeProblem
+﻿using ObjectOrientedPracticeProblem.InventoryDataManagment;
+
+namespace ObjectOrientedPracticeProblem
 {
     internal class Program
     {
+        const string INVENTORY_DATA_FILE_PATH = @"E:\Git\GitRepository\ObjectOrientedPracticeProblem\ObjectOrientedPracticeProblem\ObjectOrientedPracticeProblem\InventoryDataManagment\Inventory.json";
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Inventory Mangement by using OOPS");
+            while (true)
+            {
+                Console.WriteLine("Select Programs\n 1.Inventory Data Management");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Inventory inventory = new Inventory();
+                        inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+                        break;
+                    default:
+                        Console.WriteLine("please choose correct options :");
+                        break;
+                }
+            }
         }
     }
 }
